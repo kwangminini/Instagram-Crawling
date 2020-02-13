e236a1e26d18e40bd620c187f85fcc2317786ccf
kwangminini <rhkdals7362@gmail.com>
2020-02-13 23:26:05 +0900

    listToCsv

--- Test/listToCSV.py
+++ Test/listToCSV.py
@@ -1,1 +1,7 @@
 import csv
+data = [[1,2,3,4],[5,6,7,8]]
+csvfile=open("저장할 csv 파일 경로","w",newline="")
+csvwriter=csv.writer(csvfile)
+for row in data:
+    csvwriter.writerow(row)
+csvfile.close();
